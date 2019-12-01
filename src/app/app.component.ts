2d522044772569b2ca8586f151ccad6f0611b519
Lyubomyr <hnativ@i.ua>
2019-12-01 19:07:02 +0200

    EX-0001: Gets users with local store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { IAppState } from './store/state/app.state';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'ngrx-redux';
-  mas = ['t1', 't2', 't3'];
+  users$ = this.store.select((state) => state.user.users);
+
+  constructor(private store: Store<IAppState>) {}
+
+  ngOnInit(): void {
+
+  }
+
+
 }
